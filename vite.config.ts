52b84f865790d07e43aedef23c43b6f429882507
Thejana Hirumal <thejanahirumal5@gmail.com>
2025-03-01 11:29:30 +0530

    setup package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Thejana_Walisundara-portfolio/", // Set the correct base path for GitHub Pages
   define: {
     'process.env': {}, // Fix "process is not defined" error
   },
